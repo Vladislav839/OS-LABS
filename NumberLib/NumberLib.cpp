0ca3f4be3f269b45a3d87c9da7bc395f5414bbcb
Vladislav839 <55688058+Vladislav839@users.noreply.github.com>
2021-03-16 16:16:14 +0300

    Add files via upload

---
+++ NumberLib/NumberLib.cpp
@@ -0,0 +1,60 @@
+﻿// NumberLib.cpp : Определяет экспортируемые функции для DLL.
+//
+
+#include "framework.h"
+#include "NumberLib.h"
+
+NUMBERLIB_API Number one;
+NUMBERLIB_API Number zero;
+
+Number::Number(double data)
+{
+	value = data;
+}
+
+Number::Number()
+{
+	value = 0.0;
+}
+
+Number Number::operator+(const Number& num)
+{
+	Number res(0.0);
+	res.value = value + num.value;
+	return res;
+}
+
+Number Number::operator-(const Number& num)
+{
+	Number res(0.0);
+	res.value = value + num.value;
+	return res;
+}
+
+Number Number::operator*(const Number& num)
+{
+	Number res(0.0);
+	res.value = value * num.value;
+	return res;
+}
+
+Number Number::operator/(const Number& num)
+{
+	Number res(0.0);
+	res.value = value / num.value;
+	return res;
+}
+
+Number::~Number()
+{
+}
+
+double Number::getValue()
+{
+	return this->value;
+}
+
+NUMBERLIB_API Number CreateNumber(double value)
+{
+	return Number(value);
+}
---
+++ NumberLib/dllmain.cpp
@@ -0,0 +1,23 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "framework.h"
+#include "NumberLib.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        zero = Number(0.0);
+        one = Number(1.0);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
