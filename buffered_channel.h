03bdd79ae2b372222b42875c6fa430d976abb11d
Vladislav839 <55688058+Vladislav839@users.noreply.github.com>
2021-04-02 23:17:52 +0300

    Update buffered_channel.h

--- buffered_channel.h
+++ buffered_channel.h
@@ -13,7 +13,8 @@ private:
     int size;
     std::queue<T> buffer_queue;
     std::mutex mt;
-    std::condition_variable condition;
+    std::condition_variable condition_1;
+    std::condition_variable condition_2;
     bool open = true;
 
 public:
@@ -21,20 +22,20 @@ public:
 
     void Send(T value) {
         std::unique_lock<std::mutex> lock(mt);
-        condition.wait(lock, [this] { return buffer_queue.size() != size || !open; });
+        condition_1.wait(lock, [this] { return buffer_queue.size() != size || !open; });
         if (!open) throw(std::runtime_error("Can't send data to closed channel"));
         buffer_queue.push(value);
         lock.unlock();
-        condition.notify_one();
+        condition_2.notify_all();
     }
 
     std::pair<T, bool> Recv() {
         std::unique_lock<std::mutex> lock(mt);
-        condition.wait(lock, [this] { return !buffer_queue.empty() || !open; });
+        condition_2.wait(lock, [this] { return !buffer_queue.empty() || !open; });
         T value = buffer_queue.empty() ? T() : buffer_queue.front();
         if(!buffer_queue.empty())  buffer_queue.pop();
         lock.unlock();
-        condition.notify_one();
+        condition_1.notify_all();
         return buffer_queue.empty() ? std::make_pair(T(), false) :
             std::make_pair(value, true);
     }
@@ -43,7 +44,8 @@ public:
         std::unique_lock<std::mutex> lock(mt);
         open = false;
         lock.unlock();
-        condition.notify_all();
+        condition_1.notify_all();
+        condition_2.notify_all();
     }
 
     int Size() {
